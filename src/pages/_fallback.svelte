dc61dc78e49034f4fb8d0398c3b0f91df1432ce5
symsmith <symeon.smith@gmail.com>
2020-10-18 22:42:38 +0200

    Add 404 page

---
+++ src/pages/_fallback.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { url, goto } from '@sveltech/routify'
+    import {metatags} from '@sveltech/routify';
+    metatags.title = "Projects - Page not found";
+
+    $goto('/404');
+
+
+</script>
+
+<h1 class="title">404 - Page not found</h1>
+
+<a href={$url('/')}>Go back</a>

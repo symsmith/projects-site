b274d6fd0a7656393f34b0f0284020ed5792be8e
symsmith <symeon.smith@gmail.com>
2020-11-09 21:46:51 +0100

    Add Microblog

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -7,4 +7,5 @@
 
 <ul>
     <li><a href="./todo">To-do</a> – Simple to-do app made with <a href="https://svelte.dev">Svelte</a></li>
+    <li><a href="http://microblog.sym.codes">Microblog</a> – Twitter clone made with <a href="https://flask.palletsprojects.com/en/1.1.x/">Flask</a></li>
 </ul>

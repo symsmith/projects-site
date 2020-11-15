eaa1f19a85d2ec18c31860ddee364f3f241f066b
symsmith <symeon.smith@gmail.com>
2020-11-15 15:49:17 +0100

    Add github links

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -6,6 +6,6 @@
 <h1 class="title">Projects</h1>
 
 <ul>
-    <li><a href="./todo">To-do</a> – Simple to-do app made with <a href="https://svelte.dev">Svelte</a></li>
-    <li><a href="http://microblog.sym.codes">Microblog</a> – Twitter clone made with <a href="https://flask.palletsprojects.com/en/1.1.x/">Flask</a></li>
+    <li><strong><a href="./todo">To-do</a></strong> – Simple to-do app made with <a href="https://svelte.dev">Svelte</a> &mdash;&nbsp;<a href="https://github.com/symsmith/projects-site" target="_BLANK">Github</a></li>
+    <li><strong><a href="http://microblog.sym.codes">Microblog</a></strong> – Twitter clone made with <a href="https://flask.palletsprojects.com/en/1.1.x/">Flask</a> &mdash;&nbsp;<a href="https://github.com/symsmith/microblog" target="_BLANK">Github</a></li>
 </ul>

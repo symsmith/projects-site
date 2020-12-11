f76e4fcabee15973add0f563293bb675ef383695
Syméon Smith <symeon.smith@gmail.com>
2020-12-11 19:45:35 +0100

    Convert to 4 spaces

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { Router } from "@sveltech/routify";
-	import { routes } from "@sveltech/routify/tmp/routes";
+    import { Router } from "@sveltech/routify";
+    import { routes } from "@sveltech/routify/tmp/routes";
 </script>
 
 <section class="section columns is-centered">
     <div class="container content column is-one-third">
-      <Router {routes} />
+        <Router {routes} />
     </div>
 </section>
